f91e38d31758310bffbaa6fae2b11d4437589d7c
Health5C_M1 <health5c@Health5C-M1s-Mac-mini.local>
2015-06-23 16:45:02 +0530

    Initial Commit

---
+++ testigapp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testigapp
+//
+//  Created by Health5C_M1 on 6/23/15.
+//  Copyright (c) 2015 Health5C_M1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ testigapp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  testigapp
+//
+//  Created by Health5C_M1 on 6/23/15.
+//  Copyright (c) 2015 Health5C_M1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
