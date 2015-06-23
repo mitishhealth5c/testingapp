2d334c80a7261df7f64cddb593e2783cb01ac6b2
Health5C_M1 <health5c@Health5C-M1s-Mac-mini.local>
2015-06-23 16:57:55 +0530

    add careconnect2

---
+++ testigapp/careconnect2.h
@@ -0,0 +1,13 @@
+//
+//  careconnect2.h
+//  testigapp
+//
+//  Created by Health5C_M1 on 6/23/15.
+//  Copyright (c) 2015 Health5C_M1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface careconnect2 : UIViewController
+
+@end
---
+++ testigapp/careconnect2.m
@@ -0,0 +1,37 @@
+//
+//  careconnect2.m
+//  testigapp
+//
+//  Created by Health5C_M1 on 6/23/15.
+//  Copyright (c) 2015 Health5C_M1. All rights reserved.
+//
+
+#import "careconnect2.h"
+
+@interface careconnect2 ()
+
+@end
+
+@implementation careconnect2
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
