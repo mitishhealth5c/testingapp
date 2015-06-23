827f7a2e32bdc6475b53a00f684291748046039f
Health5C_M1 <health5c@Health5C-M1s-Mac-mini.local>
2015-06-23 17:08:21 +0530

    adding new file

---
+++ testigapp/wertyu.h
@@ -0,0 +1,13 @@
+//
+//  wertyu.h
+//  testigapp
+//
+//  Created by Health5C_M1 on 6/23/15.
+//  Copyright (c) 2015 Health5C_M1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface wertyu : UIViewController
+
+@end
